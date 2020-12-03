794cf1a22985eac341e650534fda59b3d8a09ea3
mike allen <mike@mikeapolis.com>
2020-12-02 22:55:35 -0600

    Establish a project.

---
+++ aoc/util.py
@@ -0,0 +1,23 @@
+import os.path
+from time import perf_counter
+
+
+def timeit(func):
+    """Print the time of the function call when it returns."""
+    def time_wrap(*args, **kwargs):
+        start = perf_counter()
+        returned = func(*args, **kwargs)
+        print('{name}() took {elapsed:.3f} seconds.'.format(name=func.__name__,
+                                                            elapsed=perf_counter() - start))
+        return returned
+    return time_wrap
+
+
+def load_inputs_for_source(source_file):
+    base, file = os.path.split(source_file)
+    input_file = 'input{}.txt'.format(file.split('.')[0][-2:])
+    input_path = '/'.join((base, 'input', input_file))
+
+    with open(input_path, 'r') as f:
+        return [line.split() for line in f.readlines()]
+
---
+++ aoc/y2020/day02.py
@@ -0,0 +1,16 @@
+from aoc.util import timeit
+
+
+@timeit
+def part1():
+    pass
+
+
+@timeit
+def part2():
+    pass
+
+
+if __name__ == '__main__':
+    part1()
+    part2()
